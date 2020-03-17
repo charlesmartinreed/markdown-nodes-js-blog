784582e767e9616d3a1e7e5e269ebcc61a7e527d
charlesmartinreed <charlesmartinreed@icloud.com>
2020-03-16 21:40:23 -0500

    Create _form_fields ejs partial. Create form POST for new article.

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 const articleRouter = require("./routes/articles");
 const Article = require("./models/article");
+
+// DB SETUP
+mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log('connected successfully to DB')
+})
 
 // VIEW ENGINE SETUP -- EJS
 app.set("view engine", "ejs");
 
-// ROUTES
-app.use("/articles", articleRouter);
+// MIDDLEWARE
+// allows us to access our article form's body data
+app.use(express.urlencoded({extended: false}))
 
 app.get("/", (req, res) => {
   const articles = [
-    new Article({
-    title: "Test Article",
-    description: "Test Description"
-  }), 
-    new Article({
-    title: "Test Article 2",
-    description: "Another Description"
-  })
-];
+    {
+      title: 'test article',
+      createdAt: new Date(),
+      description: 'test description'
+    }
+  ]; 
   res.render("articles/index", { articles: articles });
 });
+
+// ROUTES
+app.use("/articles", articleRouter);
 
 app.listen(5000);
