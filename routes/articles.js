69b470a611f624f395184c2e31f33f4b98f30b14
charlesmartinreed <charlesmartinreed@icloud.com>
2020-03-17 01:00:09 -0500

    Add edit functionality.

--- routes/articles.js
+++ routes/articles.js
@@ -23,25 +23,43 @@ router.get("/:slug", async (req, res) => {
   res.render("articles/show", { article: article });
 });
 
-router.post("/", async (req, res) => {
-  // create the new article
-  let article = new Article({
-    title: req.body.title,
-    description: req.body.description,
-    markdown: req.body.markdown
-  });
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article;
+    article.title = req.body.title
+    article.description = req.body.description
+    article.markdown = req.body.markdown
 
   // save the new article
   try {
     article = await article.save();
     res.redirect(`/articles/${article.slug}`);
   } catch (error) {
-    // return to the article creation page, with the previously entered data added
-    res.render("articles/new", { article: article });
+    // return to either the article creation page, with the previously entered data added or the edited page
+    res.render(`articles/${path}`, { article: article });
   }
-});
+  }
+}
 
-// DELETE
+// PUT -- UPDATE ARTICLE BY ID
+router.get('/edit/:id', async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  res.render('articles/edit', { article: article });
+})
+
+router.put('/:id', async (req, res, next) => {
+  req.article = await Article.findById(req.params.id);
+  next()
+}, saveArticleAndRedirect('edit'))
+
+// POST -- CREATE NEW ARTICLE
+
+router.post("/", async (req, res, next) => {
+  req.article = new Article() // create new article and pin it to the request
+  next() //moves on to the saveArticleAndRedirect function
+}, saveArticleAndRedirect('new'));
+
+// DELETE -- REMOVE ARTICLE BY ID
 router.delete("/:id", async (req, res) => {
   await Article.findByIdAndDelete(req.params.id);
   res.redirect("/");
